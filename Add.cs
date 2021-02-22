225a3322a84becbb10e1f6aeb14f8fcd12f1cbb5
unknown <ana.nalbandyan@gmail.com>
2021-02-22 13:59:20 -0800

    test methods added

--- Add.cs
+++ Add.cs
@@ -12,7 +12,6 @@ namespace CustomListUnitTest
         {
 
             // checking the index of the item on your CustomList<T> to make sure it was added
-            // check the count of the list to see if it was added
             // check the capacity of our list to make sure it increases when we add an item beyond the original capacity
 
             //Arrange
@@ -30,7 +29,70 @@ namespace CustomListUnitTest
             // Assert
             Assert.AreEqual(expected, actual);
         }
-      
+        [TestMethod]
+        public void Add_ItemToIndex_IndexCapacityFour()
+        {
+
+            CustomList<int> collectionOfNumbers = new CustomList<int>();
+
+            int number = 9;
+            int expected = 4;
+            int actual;
+
+            // Act
+
+            collectionOfNumbers.Add(number);
+            actual = collectionOfNumbers.Capacity;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_ItemToIndex_IndexAtZero()
+        {
+
+            CustomList<int> collectionOfNumbers = new CustomList<int>();
+
+            int number = 9;
+            int expected = 9;
+            int actual;
+
+            // Act
+
+            collectionOfNumbers.AddMethod(number);
+            actual = collectionOfNumbers[0];
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_ItemToIndex_IndexAtZero()
+        {
+
+            CustomList<int> collectionOfNumbers = new CustomList<int>();
+
+            int number = 9;
+            int number1 = 10;
+            int number2 = 11;
+            int number3 = 12;
+            int number4 = 13;
+            int expected = 9;
+            int actual;
+
+            // Act
+
+            collectionOfNumbers.AddMethod(number);
+            collectionOfNumbers.AddMethod(number1);
+            collectionOfNumbers.AddMethod(number2);
+            collectionOfNumbers.AddMethod(number3);
+            collectionOfNumbers.AddMethod(number4);
+            actual = collectionOfNumbers[0];
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
 
--- Remove.cs
+++ Remove.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using CustomListUnitTestProj;
 
 namespace CustomListUnitTest
 {
     class Remove
     {
+      
     }
 }
