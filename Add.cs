b37fb31412b3f402df9d09d1d2ff7195544b2d94
unknown <ana.nalbandyan@gmail.com>
2021-02-22 12:11:51 -0800

    init

---
+++ Add.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CustomListUnitTestProj;
+
+namespace CustomListUnitTest
+{
+    [TestClass]
+    public class Add
+    {
+        [TestMethod]
+        public void Add_Item_CountIncreasesByOne()
+        {
+
+            // checking the index of the item on your CustomList<T> to make sure it was added
+            // check the count of the list to see if it was added
+            // check the capacity of our list to make sure it increases when we add an item beyond the original capacity
+
+            //Arrange
+            CustomList<int> collectionOfNumbers = new CustomList<int>();
+
+            int number = 9;
+            int expected = 1;
+            int actual;
+
+            // Act
+
+            collectionOfNumbers.Add(number);
+            actual = collectionOfNumbers.Count;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+      
+    }
+}
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomListUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("CustomListUnitTest")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1a292cbe-19d0-43ef-9d55-85170eb7d0ab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
