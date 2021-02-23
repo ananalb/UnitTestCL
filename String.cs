6c67a2041a212c78ff6f5a3772b85e706c45fe47
unknown <ana.nalbandyan@gmail.com>
2021-02-22 18:13:12 -0800

    added string method

---
+++ String.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CustomListUnitTestProj;
+
+namespace CustomListUnitTest
+{
+    class String
+    {
+        [TestMethod]
+        public void Add_NameToIndex_NameAtIndexZero()
+        {
+
+            CustomList<string> collectionOfNumbers = new CustomList<string>();
+
+            string name = "Lara";
+            string expected = "Lara";
+            string actual;
+
+            // Act           
+            collectionOfNumbers.AddMethod(name);
+            actual = collectionOfNames[0];
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
