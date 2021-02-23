47fbe8794bd12a730865fdee562675becd437a58
unknown <ana.nalbandyan@gmail.com>
2021-02-23 07:33:50 -0800

    remove test2

--- Remove.cs
+++ Remove.cs
@@ -28,5 +28,30 @@ namespace CustomListUnitTest
             // Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Remove_ItemFromIndex_IndexCapacityFour()
+        {
+
+            CustomList<int> collectionOfNumbers = new CustomList<int>();
+
+            int number = 9;
+            int number1 = 10;
+            int number2 = 11;
+            int expected = 4;
+            int actual;
+
+            // Act
+
+            collectionOfNumbers.AddMethod(number);
+            collectionOfNumbers.AddMethod(number1);
+            collectionOfNumbers.AddMethod(number2);
+            collectionOfNumbers.RemoveMethod(number1);
+            collectionOfNumbers.RemoveMethod(number2);
+            actual = collectionOfNumbers.Capacity;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
