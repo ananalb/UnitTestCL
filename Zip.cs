f762d6b739895d3210ae77f3195b750313ea7e21
unknown <ana.nalbandyan@gmail.com>
2021-03-01 09:08:49 -0800

    zip test two and three

--- Zip.cs
+++ Zip.cs
@@ -9,7 +9,7 @@ namespace CustomListUnitTest
     public class Zip
     {
         [TestMethod]
-        public void Zip_ListOneandListTwo_ResultIsListOneandListTwo()
+        public void Zip_ListOneandListTwo_ResultIs123456()
         {
             CustomList<int> one = new CustomList<int>() { 1, 3, 5 };
             CustomList<int> two = new CustomList<int>() { 2, 4, 6 };
@@ -20,12 +20,24 @@ namespace CustomListUnitTest
             Assert.AreEqual(expected.ToString(), actual.ToString());
         }
         [TestMethod]
-        public void Zip_ListTwoIsLongerThanListOne_ResultIsListOneandListTwo()
+        public void Zip_ListTwoIsLongerThanListOne_2143658()
         {
             CustomList<int> one = new CustomList<int>() { 1, 3, 5 };
-            CustomList<int> two = new CustomList<int>() { 2, 4, 6 , 7 };
+            CustomList<int> two = new CustomList<int>() { 2, 4, 6 , 8 };
 
-            CustomList<int> expected = new CustomList<int> { 2,1,4,3,6,5,7 };
+            CustomList<int> expected = new CustomList<int> { 2,1,4,3,6,5,8 };
+            CustomList<int> actual = CustomList<int>.Zip(one, two);
+
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+
+        [TestMethod]
+        public void Zip_ListOneIsLongerThanListTwo_ResultIs1234567()
+        {
+            CustomList<int> one = new CustomList<int>() { 1, 3, 5, 7 };
+            CustomList<int> two = new CustomList<int>() { 2, 4, 6};
+
+            CustomList<int> expected = new CustomList<int> {1,2,3,4,5,6,7};
             CustomList<int> actual = CustomList<int>.Zip(one, two);
 
             Assert.AreEqual(expected.ToString(), actual.ToString());
