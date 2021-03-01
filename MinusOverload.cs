bc249a7688fe4a2750de1e692cd54312eeeea4c9
unknown <ana.nalbandyan@gmail.com>
2021-03-01 09:03:13 -0800

    zip method 1

--- MinusOverload.cs
+++ MinusOverload.cs
@@ -50,29 +50,14 @@ namespace CustomListUnitTest
         {
 
             //arrange
-            CustomList<int> one = new CustomList<int>();
-            CustomList<int> two = new CustomList<int>();
+            CustomList<int> one = new CustomList<int>() { 1, 3, 5 };
+            CustomList<int> two = new CustomList<int>() { 3, 7, 1 };
 
-
-            int number = 1;
-            int number1 = 3;
-            int number2 = 5;
-            int number3 = 7;
             CustomList<int> expected = new CustomList<int>() { 7 };
             CustomList<int> actual;
 
             // act
-
-            one.Add(number); //1
-            one.Add(number1); //3
-            one.Add(number2);//5
-            two.Add(number1); //3
-            two.Add(number3); //7
-            two.Add(number); //1
-
-
             actual = two - one;
-
 
             // assert
             Assert.AreEqual(expected.ToString(), actual.ToString());
---
+++ Zip.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListUnitTestProj;
+
+namespace CustomListUnitTest
+{
+    [TestClass]
+    public class Zip
+    {
+        [TestMethod]
+        public void Zip_ListOneandListTwo_ResultIsListOneandListTwo()
+        {
+            CustomList<int> one = new CustomList<int>() { 1, 3, 5 };
+            CustomList<int> two = new CustomList<int>() { 2, 4, 6 };
+
+            CustomList<int> expected = new CustomList<int> { 1, 2, 3, 4, 5, 6 };
+            CustomList<int> actual = CustomList<int>.Zip(one, two);
+
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+        [TestMethod]
+        public void Zip_ListTwoIsLongerThanListOne_ResultIsListOneandListTwo()
+        {
+            CustomList<int> one = new CustomList<int>() { 1, 3, 5 };
+            CustomList<int> two = new CustomList<int>() { 2, 4, 6 , 7 };
+
+            CustomList<int> expected = new CustomList<int> { 2,1,4,3,6,5,7 };
+            CustomList<int> actual = CustomList<int>.Zip(one, two);
+
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+    }
+}
